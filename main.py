d6e59860183cbbf1c5fba0eb6a57559655e5e56a
Fernando Arévalo <fernando.arevalo@uc.cl>
2025-05-30 21:34:16 -0400

    fix: modelo funciona con costo por movilizacion

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ class Model:
     def build_model(self):
         # conjuntos
         I = 8   # sectores
-        C = 50  # carabineros
-        T = 10  # días
+        C = 800  # carabineros
+        T = 365  # días
         E = 6   # especialidades
 
         # parámetros monetarios
@@ -27,23 +27,22 @@ class Model:
         k = self.data_loader.load_data('keit.csv', ['Especialidad', 'Sector', 'Dia'], 'keit')
         q = self.data_loader.load_data('qeit.csv', ['Especialidad', 'Sector', 'Dia'], 'qeit')
         u = self.data_loader.load_data('ueit.csv', ['Especialidad', 'Sector', 'Dia'], 'ueit')
-        g = self.data_loader.load_data('geikt.csv', ['Dia','Desde','Hacia','Costo'], 'geikt')
+        g = self.data_loader.load_data('geikt.csv', ['Especialidad', 'Desde', 'Hacia', 'Dia'], 'geikt')
         # parámetros de especialidad
         z = self.data_loader.load_data('zce.csv', ['Carabinero', 'Especialidad'], 'zce')
-        d = 40
+        d = 294
 
         # variables de decisión
-        x = self.model.addVars(range( E), range(I), range( T), vtype=GRB.CONTINUOUS, name="x")
-        y = self.model.addVars(range( C), range( I), range( T), vtype=GRB.BINARY, name="y")
-        w = self.model.addVars(range( I), range( T), vtype=GRB.CONTINUOUS, name="w")
-        V = self.model.addVars(range( E), range( I), range( I), range( T), vtype=GRB.CONTINUOUS, name="V")
-
+        x = self.model.addVars(range(E), range(I), range( T), vtype=GRB.CONTINUOUS, name="x")
+        y = self.model.addVars(range(C), range( I), range( T), vtype=GRB.BINARY, name="y")
+        w = self.model.addVars(range(I), range( T), vtype=GRB.CONTINUOUS, name="w")
+        V = self.model.addVars(range(E), range( I), range( I), range( T), vtype=GRB.CONTINUOUS, name="V")
         # función objetivo
         self.model.setObjective(
             gp.quicksum((c[e][i][t] + s[e][t]) * x[e, i, t] for e in range( E) for i in range( I) for t in range( T)) +
             gp.quicksum(n[e] * z[m][e] * y[m, i, t] for m in range( C) for i in range( I) for e in range( E) for t in range( T)) -
             gp.quicksum(w[i, t] * b[i] for i in range( I) for t in range( T)) + 
-            gp.quicksum((g[e][o][i][t] * V[e, o, i, t]) for e in E for o in I for t in T for i in I if i != o),
+            gp.quicksum((g[e][o][i][t] * V[e, o, i, t]) for e in range(E) for o in range(I) for i in range(I) for t in range(T) if i != o),
             GRB.MINIMIZE
         )
 
@@ -54,7 +53,7 @@ class Model:
         self.model.addConstrs(
             gp.quicksum((c[e][i][t] + s[e][t]) * x[e, i, t] for e in range( E) for t in range( T)) +
             gp.quicksum(n[e] * z[m][e] * y[m, i, t] for m in range( C) for e in range( E) for t in range( T)) + 
-            gp.quicksum((g[e][o][i][t] * V[e, o, i, t]) for e in E for o in I for t in T for i in I if i != o) 
+            gp.quicksum((g[e][o][i][t] * V[e, o, i, t]) for e in range(E) for o in range(I) for t in range(T) for i in range(I) if i != o) 
             <= f[i] for i in range( I)
         )
 
